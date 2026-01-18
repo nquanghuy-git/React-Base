f016e8db158e014374b4ae1f588601fe49c462a3
Nguyễn Quang Huy <nquanghuycv@gmail.com>
2026-01-18 22:29:22 +0700

    first

--- src/components/learn/SecondComponent.jsx
+++
@@ -1,9 +0,0 @@
-const ThirdComponent = () => {
-  return <div>Third Component</div>;
-};
-
-const SecondComponent = () => {
-  return <div className="secondComponent">Second Component</div>;
-};
-
-export { SecondComponent, ThirdComponent };
